fed70c73f01c574a57d7130f8e11c7d7029209c3
Nikhil Verma <nikhilweee@gmail.com>
2025-06-13 20:24:31 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# amtrak-live
+
+A mobile app to keep track of Amtrak trains. Built using
+[Flutter](https://flutter.dev/).
---
+++ android/app/src/main/kotlin/com/nippyapps/amtraklive/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nippyapps.amtraklive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
